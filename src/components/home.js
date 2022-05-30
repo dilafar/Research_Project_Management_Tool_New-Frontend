c98fb3ac57aaebf720fcb3437370d76419e77b32
dilafar <it20784720@my.sliit.lk>
2022-05-31 01:31:04 +0530

    slider added

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import HomeNavbar from '../common/homeNav/HomeNavbar';
+import Slider from '../common/homeNav/Slider';
 
 
 export const Home = () => {
   return (
+    <div>
     <div className="shadow p-3 mb-3 bg-white rounded" >
      <HomeNavbar/>
+    </div>
+    <Slider/>
     </div>
   )
 }
