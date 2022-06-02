aa89a6c5428269eb2d7b5f44fe4e6c26afde4938
dilafar <it20784720@my.sliit.lk>
2022-06-02 19:23:08 +0530

    added studentgroup reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,7 +5,8 @@ import staff from './staff';
 import panel from './panel';
 import request from './request';
 import response from './response';
+import sgroup from './studentgroup';
 
 export default combineReducers({
-    auth , user , staff ,panel , request ,response
+    auth , user , staff ,panel , request ,response , sgroup
 });
---
+++ src/reducers/studentgroup.js
@@ -0,0 +1,41 @@
+import { FETCH_ALL_GROUP , FETCH_GROUP_STUDENT , FETCH_GROUP_SUPERVISOR ,FETCH_GROUP_COSUPERVISOR , CREATE_GROUP , UPDATE_GROUP ,DELETE_GROUP} from '../constans/actionTypes';
+
+const studentGroupReducer = (state = {groups:[]} , action) => {
+  switch (action.type) {
+    case FETCH_ALL_GROUP:
+      return{
+              ...state,
+              groups: action.payload,
+              
+      }; 
+
+    case FETCH_GROUP_STUDENT:
+        return{
+                ...state,
+                studentGroup: action.payload,
+               
+        }; 
+    case FETCH_GROUP_SUPERVISOR:
+        return{
+                    ...state,
+                supervisorGroup: action.payload,
+                   
+        }; 
+    case FETCH_GROUP_COSUPERVISOR:
+        return{
+                        ...state,
+                cosupervisorGroup: action.payload,
+                       
+        }; 
+
+    case CREATE_GROUP:
+      return { ...state , groups: [...state.groups , action.payload]};
+    case UPDATE_GROUP:
+      return { ...state , groups: state.groups.map((groupx)=> (groupx._id === action.payload._id ? action.payload : groupx))};
+    case DELETE_GROUP:
+      return { ...state , groups: state.groups.filter((groupx)=> groupx._id !== action.payload)};
+    default:
+      return state;
+  }
+};
+export default studentGroupReducer;
